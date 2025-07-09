a37b5551c04419b885fcd1cdaa5b1602dce28e6b
Jannatun Nur Niha <jannatunnurniha@gmail.com>
2025-07-10 00:08:08 +0600

    Delete Module - 01/Problem 01.py

--- Module - 01/Problem 01.py
+++
@@ -1,9 +0,0 @@
-# A program to check if the input number is odd or even
-
-
-Number = int(input())
-
-if Number % 2 != 0:
-    print(f"{Number} is odd number")
-else:
-    print(f"{Number} is even number")
