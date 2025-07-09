3b17d51ff0871dd100678867bb58e20a029e2058
Jannatun Nur Niha <jannatunnurniha@gmail.com>
2025-07-10 00:06:37 +0600

    Add files via upload

---
+++ Module - 01/problem 2.py
@@ -0,0 +1,20 @@
+number1 = float(input("Enter 1st number:"))
+number2 = float(input("Enter 2nd number:"))
+operator = input("Enter an operator(+, -, *, /)")
+
+if operator == '+':
+    print('sum is:', number1+number2)
+elif operator == '-':
+    print('Difference of two numbers:', abs(number1-number2))
+elif operator == '*':
+    print('product is:', number1*number2)
+elif operator == '/' and number2 == 0:
+    print("can't divide by 0")
+elif operator == '/' and number2 != 0:
+    print('quotient is:', round(number1/number2, 2))
+else:
+    print("Invalid operator. Please enter +, -, *, or /")
+
+    
+
+
