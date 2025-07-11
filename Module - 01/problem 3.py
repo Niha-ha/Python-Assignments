8a298819cc87aefa6e64c2527a3b5fd6417f61b3
Jannatun Nur Niha <jannatunnurniha@gmail.com>
2025-07-11 11:54:19 +0600

    Update problem 3.py

--- Module - 01/problem 3.py
+++ Module - 01/problem 3.py
@@ -2,4 +2,4 @@ result = 0
 for x in range(2, 101, 2):
     result += x
     print(x)  
-print('sum of all even numbers between 1 to 100 is :', result)
+print('sum of all even numbers between 1 and 100 is :', result)
