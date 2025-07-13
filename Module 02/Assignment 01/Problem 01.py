d32c8f1a4060b2019fbb23bd2645d8b2c02ea89e
Jannatun Nur Niha <jannatunnurniha@gmail.com>
2025-07-13 20:21:49 +0600

    Update Problem 01.py

--- Module 02/Assignment 01/Problem 01.py
+++ Module 02/Assignment 01/Problem 01.py
@@ -1,1 +1,12 @@
+'''Write a function that takes two numbers as input and returns their average.
+ Call the function with different values'''
 
+
+def average(number1, number2):
+    return (number1+number2)/2
+
+# calling the function with different values
+print(round(average(2.5,5.6),2))
+print(round(average(2.5,-5.6),2))
+print(round(average(-2.5,-5.6),2))
+print(round(average(100,34),2))
