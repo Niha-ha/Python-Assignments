42898012fe46c051299cd1d87e37350a6161c8c1
Jannatun Nur Niha <jannatunnurniha@gmail.com>
2025-07-24 01:35:00 +0600

    Update Inheritance and Advanced OOP Features.py

--- Module 03/Assignment 02/Inheritance and Advanced OOP Features.py
+++ Module 03/Assignment 02/Inheritance and Advanced OOP Features.py
@@ -1,1 +1,21 @@
+'''Inheritance and Advanced OOP Features
+Create a base class Animal and a derived class Dog that inherits from Animal.
+Output: 
+Animals make different sounds.           # From Animal class
+Dog barks!                               # From Dog Class'''
 
+# Base class--
+class Animal:
+   def sound(self):
+      print("Animals make different sounds")
+
+# Derived class--
+class Dog(Animal):
+         def sound(self):
+            super().sound()           # Calls the sound method from Animal
+            print("Dog barks!")
+
+
+# Create object of Dog and calls the method   
+d = Dog()  
+d.sound() 
